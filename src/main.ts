4567ffb7c7473d1accb6ef26e1dc5c0481d7e5ee
abenezer-ayalneh <abenezer.ayalneh.42@gmail.com>
2023-02-19 14:16:48 +0300

    chore: prepare the user microservice

---
+++ src/main.ts
@@ -0,0 +1,40 @@
+import { HttpStatus, ValidationError, ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import {
+  MicroserviceOptions,
+  RpcException,
+  Transport,
+} from '@nestjs/microservices';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: 3005,
+      },
+    },
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      exceptionFactory: (errors: ValidationError[]) => {
+        return errors.map((error) => {
+          throw new RpcException({
+            message: error.constraints
+              ? Object.values(error.constraints)[0]
+              : null,
+            statusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+            errors: error.constraints ? Object.values(error.constraints) : null,
+          });
+        });
+      },
+    }),
+  );
+
+  await app.listen();
+}
+bootstrap();
